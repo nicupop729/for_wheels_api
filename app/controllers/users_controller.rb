77be2c86d6f59cdbf9af82cb5a53ba0cb5bd5872
Nicolae Pop <nicupop729@gmail.com>
2022-04-01 12:33:08 -0400

    Build the logic for the User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render json: { status: 'OK', message: 'Users are here', data: @users }, status: :ok
   end
 
-  def show
+  def log_in
+    user = User.where(name: params[:name])
+    render json: { status: 'OK', message: 'User found', data: user }, status: :ok
   end
 
   def create
+    new_user = User.new(user_params)
+      if new_user.save
+        render status: 201, json: {
+        status: 'OK',
+        message: 'New user created'
+      }
+      else
+        render json: { errors: new_user.errors,
+                     status: 422 }, status: 422
+      end
+  
+  end
+
+  private
+
+  def user_params
+    params.permit(:name)
   end
 end
