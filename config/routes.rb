073231b8c465c4af5d389254d194c80ef208771a
Nicolae Pop <nicupop729@gmail.com>
2022-04-01 12:33:26 -0400

    Set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users only: [:index, :show, :create]
+  resources :users, only: [:index, :create]
+
+  post 'users/log_in', to: 'users#log_in'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
